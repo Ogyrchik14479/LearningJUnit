f89e51f6f345be5f180af08454d638b7815742ed
i.reznikov <i.reznikov@simplex-software.ru>
2021-08-17 22:01:27 +0300

    init

---
+++ src/main/java/igor/reznikov/LearnJUnit/LearnJUnitApplication.java
@@ -0,0 +1,13 @@
+package igor.reznikov.LearnJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/igor/reznikov/LearnJUnit/LearnJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.reznikov.LearnJUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
