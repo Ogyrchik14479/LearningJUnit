146c8619411accea463fa9cb8353733d372404f9
i.reznikov <i.reznikov@simplex-software.ru>
2021-08-20 23:11:02 +0300

    add SkillLevelEnum, fields in Skill

--- src/main/java/igor/reznikov/ResumeConstructor/entities/Skill.java
+++ src/main/java/igor/reznikov/ResumeConstructor/entities/Skill.java
@@ -1,6 +1,10 @@
 package igor.reznikov.ResumeConstructor.entities;
 
+import igor.reznikov.ResumeConstructor.enums.SkillLevelEnum;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
@@ -21,6 +25,12 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Entity
 @Table(name = "skills")
 public class Skill extends AbstractPersistable<Long> {
+
+    String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "skill_level")
+    SkillLevelEnum skillLevelEnum;
 
     @ManyToOne
     Resume resume;
---
+++ src/main/java/igor/reznikov/ResumeConstructor/enums/SkillLevelEnum.java
@@ -0,0 +1,9 @@
+package igor.reznikov.ResumeConstructor.enums;
+
+public enum SkillLevelEnum {
+    EXPERT,
+    EXPERIENCED,
+    SKILFUL,
+    BEGINNER,
+    NOVICE
+}
